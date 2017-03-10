7199365f16c30d6e66754107f250ac5a8090678f
Umair Ashraf <umrashrf@gmail.com>
2017-03-10 10:54:28 -0500

    ES5 way of disabling zen discovery

--- src/testing/elasticsearch5.py
+++ src/testing/elasticsearch5.py
@@ -7,3 +7,5 @@ class Elasticsearch(ES):
         super(Elasticsearch, self).initialize()
         self.DEFAULT_SETTINGS['boot_timeout'] = 60
         self.elasticsearch_yaml.pop('discovery.zen.ping.multicast.enabled')
+        self.elasticsearch_yaml['discovery.zen.ping.unicast.hosts'] = ''
+        self.elasticsearch_yaml['discovery.zen.minimum_master_nodes'] = 0
